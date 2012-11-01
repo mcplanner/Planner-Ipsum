cbfe9d944a059bfd9a24297c170a010975b7631f
Ruth Miller <ruth.mil@gmail.com>
2012-11-01 16:11:27 -0700

    Plannified

--- gga-bacon-ipsum-form.php
+++ gga-bacon-ipsum-form.php
@@ -1,22 +1,17 @@
 <?php
 /*
-Plugin Name: Bacon Ipsum - Generator Form
-Description: Generates the input form for generating meaty bacon ipsum
-Plugin URI: https://github.com/petenelson/bacon-ipsum
-Version: 2.1.1
-Author: Pete Nelson (@GunGeekATX)
-Author URI: http://petenelson.com
+Plugin Name: Planning Ipsum 
 */
 
-add_shortcode('gga-bacon-ipsum-form', 'gga_bacon_ipsum_form');
-function gga_bacon_ipsum_form($atts)
+add_shortcode('gga-planner-ipsum-form', 'gga_planner_ipsum_form');
+function gga_planner_ipsum_form($atts)
 {
 	$output = '';
 
 	$form = '
-		<p>Does your lorem ipsum text long for something a little meatier? Give our generator a try… it’s tasty!</p>
+		<p>Need some wonky planner verbage? Generate some automatically!</p>
 
-		<form id="make-it-meaty" action="' . site_url('/') . '" method="get">
+		<form id="make-it-planning" action="' . site_url('/') . '" method="get">
 			<table>
 				<tbody>
 				<tr>
@@ -25,15 +20,11 @@ function gga_bacon_ipsum_form($atts)
 				</tr>
 				<tr>
 					<td>Type:</td>
-					<td><input id="all-meat" type="radio" name="type" value="all-meat" checked="checked" /><label for="all-meat">All Meat</label> <input id="meat-and-filler" type="radio" name="type" value="meat-and-filler" /><label for="meat-and-filler">Meat and Filler</label></td>
+					<td><input id="all-planner" type="radio" name="type" value="all-planner" checked="checked" /><label for="all-planner">Planning buzzwords only</label> <input id="planning-and-filler" type="radio" name="type" value="planning-and-filler" /><label for="meat-and-filler">Buzzwords with and a little filler, too</label></td>
 				</tr>
 				<tr>
 					<td></td>
-					<td><input id="start-with-lorem" type="checkbox" name="start-with-lorem" value="1" checked="checked" /> <label for="start-with-lorem">Start with \'Bacon ipsum dolor sit amet...\'</label></td>
-				</tr>
-				<tr>
-					<td></td>
-					<td><input type="submit" value="Give me bacon" /></td>
+					<td><input type="submit" value="Give me text!" /></td>
 				</tr>
 				</tbody>
 				</table>
@@ -44,9 +35,9 @@ function gga_bacon_ipsum_form($atts)
 	if (isset($_REQUEST["type"]))
 	{
 
-		require_once 'gga-BaconIpsumGenerator.php';
+		require_once 'gga-PlannerIpsumGenerator.php';
 
-		$generator = new BaconIpsumGenerator();
+		$generator = new PlannerIpsumGenerator();
 		$number_of_paragraphs = 5;
 		if (isset($_REQUEST["paras"]))
 			$number_of_paragraphs = intval($_REQUEST["paras"]);
@@ -59,7 +50,7 @@ function gga_bacon_ipsum_form($atts)
 		if ($number_of_paragraphs > 100)
 			$number_of_paragraphs = 100;
 
-		$paragraphs = $generator->Make_Some_Meaty_Filler(
+		$paragraphs = $generator->Make_Some_Planner_Filler(
 			$_REQUEST["type"], 
 			$number_of_paragraphs,
 			isset($_REQUEST["start-with-lorem"]) && $_REQUEST["start-with-lorem"] == "1");
